58d89d0742f2834589f7e89d4f6fe7f88a7d04f2
Nikspb2022 <107208472+Nikspb2022@users.noreply.github.com>
2023-05-06 13:20:31 +0600

    добавил апп - юзер

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth, todos
+from routers import auth, todos, users
 import models
 from database import engine
 
@@ -9,3 +9,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(users.router)
